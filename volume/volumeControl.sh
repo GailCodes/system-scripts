ba64a9479bce84b71863a527a6cfddcea8baacdb
JamieBurridge <xross_dev@proton.me>
2023-01-21 15:41:49 +0000

    add volume control scripts

---
+++ volume/volumeControl.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/bash
+
+function get_volume {
+    amixer -D pulse get Master | grep 'Right:' | awk -F'[][]' '{ print $2 }'
+}
+
+function notify_volume {
+    notify-send -r 8888 -a Volume "$(get_volume)"
+}
+
+case "$1" in 
+    up)
+        amixer -D pulse sset Master 5%+
+        notify_volume
+        ;;
+    down)
+        amixer -D pulse sset Master 5%-
+        notify_volume
+        ;;
+esac
